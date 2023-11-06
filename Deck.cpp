e23a8e56a4ae5657dbe5fa1a034cdc9aaac7df89
Shelly Masih <shelly_masih22@yahoo.com>
2023-11-06 11:06:39 -0500

    changes

--- Deck.cpp
+++ Deck.cpp
@@ -1,37 +1,55 @@
-// // Shelly Masih
-// // CSCI 335
+// Shelly Masih
+// CSCI 335
 
-// #include "Deck.hpp"
+#include "Deck.hpp"
+#include <random>
 
-// Deck::Deck() { //Construct a new Deck object
+template <typename CardType>
+Deck<CardType>::Deck() { // Construct a new Deck object
+}
 
-// }
+template <typename CardType>
+Deck<CardType>::~Deck() { // Destroy the Deck object
+  cards_.clear();
+}
 
-// Deck::~Deck() { // Destroy the Deck object
+template <typename CardType>
+void Deck<CardType>::AddCard(const CardType &card) {
+  cards_.push_back(card);
+}
 
+template <typename CardType> CardType &&Deck<CardType>::Draw() {
+  if (cards_.empty()) {
+    throw std::out_of_range("Deck is empty, cannot draw a card.");
+  }
 
-// }
+  // Get the last card from the deck
+  CardType &&card = std::move(cards_.back());
 
-// void  Deck::AddCard(const CardType& card)  {
+  // Remove the last card from the deck
+  cards_.pop_back();
 
-// }
+  // Return the card as a right-hand value
+  return std::move(card);
+}
 
-// CardType&& Deck::Draw() {
+template <typename CardType> bool Deck<CardType>::IsEmpty() const {
+  return cards_.empty();
+}
 
-// }
+template <typename CardType> void Deck<CardType>::Shuffle() {
+  // Create a random number generator with a fixed seed (2028358904)
+  std::mt19937 rng(2028358904);
 
-// bool Deck::isEmpty() {
+  // Shuffle the cards using the random number generator
+  std::shuffle(cards_.begin(), cards_.end(), rng);
+}
 
-// }
+template <typename CardType> int Deck<CardType>::getSize() const {
+  return cards_.size();
+}
 
-// void Deck::Shuffle() {
-
-// }
-
-// int Deck::getSize() {
-
-// }
-
-// std::vector<CardType> Deck::getDeck() const {
-    
-// }
+template <typename CardType>
+std::vector<CardType> Deck<CardType>::getDeck() const {
+  return cards_;
+}
--- Hand.cpp
+++ Hand.cpp
@@ -1,44 +1,64 @@
-// // Shelly Masih
-// // CSCI 335
+// Shelly Masih
+// CSCI 335
 
-// #include "Hand.hpp"
+#include "Hand.hpp"
 
-// Hand::Hand() { // Construct a new Hand object
+Hand::Hand() { // Construct a new Hand object
+}
 
-// }
+Hand::~Hand() { // Destroy the Hand object
+  cards_.clear();
+}
 
-// Hand::~Hand() { // Destroy the Hand object
+Hand::Hand(const Hand &other) { // Copy Constructor
+  cards_ = other.cards_;
+}
 
-// }
+Hand &Hand::operator=(const Hand &other) {
+  if (this != &other) {
+    cards_ = other.cards_;
+  }
+  return *this;
+}
 
-// Hand::Hand(const Hand& other) { // Copy Constructor
+Hand::Hand(Hand &&other) { // move constructor
+  cards_ = std::move(other.cards_);
+}
 
-// }
+Hand &Hand::operator=(Hand &&other) {
+  if (this != &other) { // Check for self-assignment
+    // Move the contents of other.cards_ to this->cards_
+    cards_ = std::move(other.cards_);
+  }
+  return *this;
+}
 
-// Hand &Hand::operator=(const Hand& other) {
+const std::deque<PointCard> &Hand::getCards() const { return cards_; }
 
-// }
+void Hand::addCard(PointCard &&card) { cards_.push_back(std::move(card)); }
 
-// Hand::Hand(Hand&& other) { // move constructor
+bool Hand::isEmpty() const { return cards_.empty(); }
 
-// }
+void Hand::Reverse() {
+  std::deque<PointCard> reversedCards;
+  for (auto it = cards_.rbegin(); it != cards_.rend(); ++it) {
+    reversedCards.push_back(std::move(*it));
+  }
+  cards_ = std::move(reversedCards);
+}
 
-// const std::deque<PointCard>& Hand::getCards() const {
+int Hand::PlayCard() {
+  if (isEmpty()) {
+    throw std::logic_error("The hand is empty.");
+  }
 
-// }
+  PointCard card = std::move(cards_.front());
+  cards_.pop_front();
 
-// void Hand::addCard(PointCard&& card) {
+  if (!card.isPlayable()) {
+    // If the card is not playable, do not add its points
+    return 0;
+  }
 
-// }
-
-// bool Hand::isEmpty() const {
-
-// }
-
-// void Hand::Reverse() {
-
-// }
-
-// int Hand::PlayCard() {
-
-// }
+  return std::stoi(card.getInstruction());
+}
